5f98409df1ccb8fa1e1c138c7308134a5990bad6
Iskander Sharipov <quasilyte@gmail.com>
2023-06-17 11:47:58 +0400

    add a synth info package (soundfont metadata)

---
+++ synthdb/synthdb.go
@@ -0,0 +1,58 @@
+package synthdb
+
+import (
+	"fmt"
+
+	"github.com/sinshu/go-meltysynth/meltysynth"
+)
+
+type SoundFont struct {
+	Name string
+
+	Instruments []*Instrument
+
+	Data *meltysynth.SoundFont
+}
+
+type Instrument struct {
+	Name string
+
+	Index       int
+	PatchNumber int
+}
+
+var TimGM6mb = &SoundFont{
+	Name: "TimGM6mb",
+
+	Instruments: []*Instrument{
+		{Name: "Synth Bass 1", Index: 88},
+		{Name: "Synth Bass 2", Index: 87},
+		{Name: "Distortion Guitar", Index: 96},
+	},
+}
+
+func (sf *SoundFont) Load(data *meltysynth.SoundFont) {
+	sf.Data = data
+
+	for _, inst := range sf.Instruments {
+		instInfo := data.Presets[inst.Index]
+		inst.PatchNumber = int(instInfo.PatchNumber)
+
+		for _, region := range instInfo.Regions {
+			if region.GetVelocityRangeStart() != 0 {
+				panic("unexpected velocity range start")
+			}
+			if region.GetVelocityRangeEnd() != 127 {
+				panic("unexpected velocity range end")
+			}
+			if region.GetKeyRangeStart() != 0 {
+				panic("unexpected key range start")
+			}
+			if region.GetKeyRangeEnd() != 127 {
+				panic("unexpected key range end")
+			}
+		}
+
+		fmt.Printf("loaded %q instrument (patch name = %s)\n", inst.Name, instInfo.Name)
+	}
+}
