a9fa1b5b192881262adf38f85b54db4e87574b7b
Iskander Sharipov <quasilyte@gmail.com>
2023-06-23 11:56:29 +0400

    increase the simulation precision by making the calc dt lower

--- stage/program_runner.go
+++ stage/program_runner.go
@@ -37,7 +37,7 @@ func (r *programRunner) RunProgram(prog SynthProgram) []noteActivation {
 		r.delays = append(r.delays, inst.Period)
 	}
 
-	const stepsPerSecond = 180
+	const stepsPerSecond = 360
 	dt := 1.0 / stepsPerSecond
 	t := 0.0
 
